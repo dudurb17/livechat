94e1b444e8eae9cd11f4c1776c25abfdcb3165ac
dudurb17 <eduardo.robettibedin@gmail.com>
2024-07-15 22:48:29 -0300

    chat

--- resources/views/livewire/chatcomponent.blade.php
+++ resources/views/livewire/chatcomponent.blade.php
@@ -1,11 +1,24 @@
 <?php
 use App\Models\Message;
-use function Livewire\Volt\{state, mount};
+use App\Events\NewMessageEvent;
+use function Livewire\Volt\{state, mount, on};
 state(['messages' => []]);
+state(['message' => '']);
 mount(function () {
     $this->messages = Message::all();
 });
 
+$send = function () {
+    $message = Message::create(['message' => $this->message, 'user_id' => Auth::user()->id]);
+    $this->reset('message');
+    NewMessageEvent::dispatch($message);
+};
+
+on([
+    'echo-private:newmessage,NewMessageEvent' => function () {
+        $this->messages = Message::all();
+    },
+]);
 ?>
 
 <div>
@@ -57,9 +70,9 @@ mount(function () {
     <div class="p-4">
         <label for="hs-trailing-button-add-on" class="sr-only">Label</label>
         <div class="flex rounded-lg shadow-sm">
-            <input type="text" id="hs-trailing-button-add-on" name="hs-trailing-button-add-on"
+            <input wire:model='message' type="text" id="hs-trailing-button-add-on" name="hs-trailing-button-add-on"
                 class="py-3 px-4 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
-            <button type="button"
+            <button wire:click='send' type="button"
                 class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-e-md border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                 Button
             </button>
